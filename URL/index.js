4520289e972b218358f1d378604ae44ce26220f4
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-02-03 03:51:45 +0500

    docs: explain URL components and domain names

---
+++ URL/index.js
@@ -0,0 +1,14 @@
+// Uniform Resource Locator - URL
+
+// http://localhost:3000
+// https://booksspine.com
+
+// protocol : http or https
+// port : 3000
+// host : localhost || booksspine.com
+// path : / || /about || /contact
+// query : ?name=booksspine&age=20
+// hash : #header
+
+// what is domain name?
+// domain name is the name of the website and user-friendly address of the website
