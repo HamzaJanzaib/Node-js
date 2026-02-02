b93fb00f3c212c42442a31bdc2d347213b7e5db8
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-02-03 03:55:13 +0500

    feat(web-server): add HTTP methods reference file
    Add a new file documenting common HTTP methods (GET, POST, PUT, DELETE, PATCH) with their purposes, serving as a quick reference for the web server implementation.

---
+++ web-server/method.js
@@ -0,0 +1,13 @@
+// HTTP Methods
+
+// GET - Read
+// POST - Create
+// PUT - Update
+// DELETE - Delete
+// PATCH - Update
+
+// GET - User request any server to get data
+// POST - User request any server to create data
+// PUT - User request any server to update data
+// DELETE - User request any server to delete data
+// PATCH - User request any server to update data
