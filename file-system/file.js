ece385bd3a18ab0297fbf8434e7b45a0947436f2
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-02-03 02:44:04 +0500

    feat: add Node.js file system examples and sample contact data
    - Added contact.txt with a list of 14 sample contacts including names, phones, and emails
    - Added copy.txt as a duplicate of contact.txt for file copying demonstration
    - Added file.txt with sample content for writeFileSync example
    - Updated file.js with commented examples of fs methods (sync/async write, read, append, copy, unlink)
    - Demonstrates basic Node.js file system operations for learning purposes

--- file-system/file.js
+++ file-system/file.js
@@ -1,4 +1,41 @@
 // require the fs module
 const fs = require("fs");
+// sync write file
+// fs.writeFileSync("file.txt", "Learning Node.js file system");
 
-fs.writeFileSync("file.txt", "Learning Node.js file system");
+// async write file
+// fs.writeFile("file.txt", "Learning Node.js file system async", (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("File created");
+//   }
+// });
+
+// read file async
+// fs.readFile("contact.txt", "utf8", (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(data);
+//   }
+// });
+
+// read file sync
+// const data = fs.readFileSync("contact.txt", "utf8");
+// console.log(data);
+
+// small difference between sync and async
+// sync : block the code execution until the file is read
+// async : does not block the code execution
+
+// fs.appendFileSync("contact.txt", "14. Jennifer White - Phone: +1-555-0114 - Email: jennifer.white@email.com\n");
+
+// const data = fs.readFileSync("contact.txt", "utf8");
+// console.log(data);
+
+// fs.writeFileSync("copy.txt", "");
+
+// fs.copyFileSync("contact.txt", "copy.txt");
+
+fs.unlinkSync("hello.txt");
