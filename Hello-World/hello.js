fd1b77e96b8d544e2280605b10a740d92898fa63
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-02-03 02:14:18 +0500

    feat: Initialize a basic Node.js "Hello World" project with a `package.json` and a simple `hello.js` script.

--- Hello-World/hello.js
+++ Hello-World/hello.js
@@ -1,1 +1,10 @@
 console.log("Hello World");
+
+// window is not defined in node.js
+// creating node js for server side programming
+// example:
+// console.log(window);
+// console.log(alert);
+// etc
+
+// npm init -y create package.json file 
