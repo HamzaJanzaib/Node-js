218751782dec5c1627df7af2080dcac6bfa2760e
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-04-03 09:40:45 +0500

    creating new user

--- postgress/src/server.js
+++ postgress/src/server.js
@@ -1,12 +1,32 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const { createTables } = require('./concepts/user-queries');
+const { createTables , createUser } = require('./concepts/user-queries');
 
+async function initializeDatabase() {
+    try {
+        await createTables();
+        console.log('Database initialized successfully');
+    } catch (error) {
+        console.error('Error initializing database', error);
+        process.exit(1);
+    }   
+}
+
+async function CreateUsers () {
+    try {
+        const user1 = await
+            createUser('John Doe', "john.doe@example.com");
+        console.log('User created successfully');
+    } catch (error) {
+        console.error('Error creating user', error);
+    }
+}
 
 async function startServer() {
     try {
-        await createTables();
+        await initializeDatabase();
+        await CreateUsers();
         console.log('Server is running and ready to accept requests');
     } catch (error) {
         console.error('Error starting server', error);
