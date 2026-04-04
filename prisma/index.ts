5b774152f92bbf973d1ade15eb6d409a11b60c16
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-04-04 17:37:52 +0500

    / routes

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,12 +1,22 @@
 import { Application, Request, Response } from "express";
+import { prisma } from "./lib/prisma";
 
-const express  = require('express');
+const express = require('express');
 
-const app : Application = express();
+const app: Application = express();
 
-    
-app.get('/', (req : Request, res : Response) => {
-    res.send('Hello World!');
+
+app.get('/', (req: Request, res: Response) => {
+    prisma.user.findMany({
+        include: {
+            posts: true,
+        },
+    }).then(users => {
+        res.json(users);
+    }).catch(error => {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while fetching users' });
+    });
 });
 
 
