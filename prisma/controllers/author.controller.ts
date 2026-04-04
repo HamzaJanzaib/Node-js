47ce4ba85a314cc700b4bc94c9777ec7b62400cb
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-04-04 22:47:41 +0500

    resning proper responce

--- prisma/controllers/author.controller.ts
+++ prisma/controllers/author.controller.ts
@@ -1,23 +1,26 @@
 import { log } from "console";
 import { createAuthor, deleteAuthor, getAuthorById, getAuthors, updateAuthor } from "../services/author.service";
+import { Response } from "express";
 
-async function createAuthorController(req: any, res: any) {
+async function createAuthorController(req: any, res: Response) {
     try {
         const { name, age } = req.body;
        
         const author = await createAuthor({ name, age });
-        return author;
+       
+        res.status(201).json(author);
     } catch (error) {
         console.error(error);
         throw new Error('An error occurred while creating the author');
     }
 }
 
-async function getAuthorsController(req: any, res: any) {
+async function getAuthorsController(req: any, res: Response) {
     try {
         log('Fetching authors...');
         const authors = await getAuthors();
-        return authors;
+
+        res.json(authors);
     } catch (error) {
         console.error(error);
         throw new Error('An error occurred while fetching the authors');
@@ -25,11 +28,11 @@ async function getAuthorsController(req: any, res: any) {
 }
 
 
-async function getAuthorByIdController(req: any, res: any) {
+async function getAuthorByIdController(req: any, res: Response) {
     try {
         const { id } = req.params;
         const author = await getAuthorById(parseInt(id));
-        return author;
+        res.json(author);
     } catch (error) {   
         console.error(error);
         throw new Error('An error occurred while fetching the author');
@@ -42,17 +45,18 @@ async function updateAuthorController(req: any, res: any) {
         const { id } = req.params;
         const { name, age } = req.body;
         const author = await updateAuthor(parseInt(id), { name, age });
-        return author;
+        res.json(author);
     } catch (error) {
         console.error(error);
         throw new Error('An error occurred while updating the author');
     }
 }
 
-async function deleteAuthorController(req: any, res: any) {
+async function deleteAuthorController(req: any, res: Response) {
     try {
         const { id } = req.params;
         await deleteAuthor(parseInt(id));
+        res.json({ message: 'Author deleted successfully' });
     } catch (error) {
         console.error(error);
         throw new Error('An error occurred while deleting the author');
--- prisma/controllers/book.controller.ts
+++ prisma/controllers/book.controller.ts
@@ -1,32 +1,33 @@
+import { Response } from "express";
 import { createBook, deleteBook, getBookById, getBooks, getBooksByAuthor, updateBook } from "../services/book.service";
 
-async function createBookController(req: any, res: any) {
+async function createBookController(req: any, res: Response) {
     try {
          const { title, PublishDate, price, authorId } = req.body;
         const book = await createBook({ title, PublishDate: new Date(PublishDate), price, authorId });
 
-        return book;
+        res.json(book);
     } catch (error) {
         console.error(error);
         throw new Error('An error occurred while creating the book');
     }
 }
 
-async function getBooksController(req: any, res: any) {
+async function getBooksController(req: any, res: Response) {
     try {
         const books = await getBooks();
-        return books;
+        res.json(books);
     } catch (error) {
         console.error(error);
         throw new Error('An error occurred while fetching the books');
     }
 }
 
-async function getBooksByAuthorController(req: any, res: any) {
+async function getBooksByAuthorController(req: any, res: Response) {
     try {
         const { authorId } = req.params;
         const books = await getBooksByAuthor(parseInt(authorId));
-        return books;
+        res.json(books);
     } catch (error) {
         console.error(error);
         throw new Error('An error occurred while fetching the books by author');
@@ -34,11 +35,11 @@ async function getBooksByAuthorController(req: any, res: any) {
 }
 
 
-async function getBookByIdController(req: any, res: any) {
+async function getBookByIdController(req: any, res: Response) {
     try {
         const { id } = req.params;
         const book = await getBookById(parseInt(id));
-        return book;
+        res.json(book);
     } catch (error) {
         console.error(error);
         throw new Error('An error occurred while fetching the book');
@@ -46,23 +47,25 @@ async function getBookByIdController(req: any, res: any) {
 
 }
 
-async function updateBookController(req: any, res: any) {
+async function updateBookController(req: any, res: Response) {
     try {
         const { id } = req.params;
         const { title, PublishDate, price, authorId } = req.body;
         const book = await updateBook(parseInt(id), { title, PublishDate: new Date(PublishDate), price, authorId });
 
-        return book;
+        res.json(book);
     } catch (error) {
         console.error(error);
         throw new Error('An error occurred while updating the book');
     }
 }
 
-async function deleteBookController(req: any, res: any) {
+async function deleteBookController(req: any, res: Response) {
     try {
         const { id } = req.params;
         await deleteBook(parseInt(id));
+        res.json({ message: 'Book deleted successfully' });
+
     } catch (error) {
         console.error(error);
         throw new Error('An error occurred while deleting the book');
